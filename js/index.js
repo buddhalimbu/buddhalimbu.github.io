0dde0965aac29b1ac9da02c2f339503ad90afd21
buddhalimbu <53037805+buddhalimbu@users.noreply.github.com>
2020-01-17 23:42:03 +0545

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,70 +1,78 @@
-//All the code are writter in jquery and javascript by buddha limbu
 $(document).ready(function(){
-$('#dark').click(function(){
-	if ($(this).text() == 'Dark'){
-		$(this).text('Light').css({'background':'#323232','color':'#eee',});
-	}
-	else {
-		$(this).text("Dark").css({'background':'#eee','color':'#283592'});
-	};
-});
-
-//prend bubles
-$('.header').prepend('<div class="bubble b1"></div><div class="bubble b2"></div><div class="bubble b3"></div><div class="bubble b4"></div><div class="bubble b5"></div><div class="bubble b6"></div><div class="bubble b7"></div><div class="bubble b8"></div><div class="bubble b9"></div><div class="bubble b10"></div>');
-$('.header').append('<div class="line"/><div class="line1"/><div class="line2"/>');
-
-//add fa-fw in span
 $('.hobby-item span').wrap('<div class="wrap-span"/>');
 $('.slider').wrap('<div class="wrapper"/>');
 $('.slider q').wrap('<div class="quote-box"/>');
 $('.q').prepend('<span class="fa fa-heart"/>');
 $('.logo')['clone']()['appendTo']('.logo-footer');
 $('.credit').wrap('<div class="container"/>');
+$('.demo a').wrap('<button class="demo-button"/>');
+$('.image-wrapper').append('<div class="myname"><span class="greet">Welcome</span><h1>Buddha</p></div>');
 
-//menu toggle
-$('.bars')['on']('click',function(){
-	$('body')['toggleClass']('toggle-menu');
-	if($(this)['children']('span')['hasClass']('fa-bars')){
-		$(this)['children']('span')['addClass']('fa-times').css({'transition':'0.7s ease'});
-		$(this)['children']('span')['removeClass']('fa-bars');
-	}
-	else {
-		$(this)['children']('span')['removeClass']('fa-times');
-		$(this)['children']('span')['addClass']('fa-bars');
-	}
-});
-$('.menu-link a')['addClass']('scroll-menu');
-
+//add class to menu-link a that when user clicks on it the body will animate
+$('.topnav a')['addClass']('scroll-menu');
+$('.topnav').prepend('<span class="fa fa-times"></span>');
+//create sticky navbar on scroll 
 $(window).scroll(function() {
 	if ($(window).scrollTop() >= 10) {
 		$('.navbar').addClass('sticky');
-		$('.menu-link').addClass('same-bg');
-
+		$('.menu-icon .fa-bars').css({'background':'#1B2631'})
+		$('.nightmode').css({'background':'#B5AD07'});
+		$('.themepack').css({'background':'#1B2631'});
 	}
 	else {
 		$('.navbar').removeClass('sticky');
-		$('.menu-link').removeClass('same-bg');
+		$('.menu-icon .fa-bars').css({'background':'rgba(255,255,255,0.06'});
+		$('.nightmode,.themepack').css({'background':'rgba(255,255,255,0.06'})
 	}
 });
-$('.scroll-menu').click(function(){
+$(window).scroll(function() {
+	if ($(window).scrollTop() >= 2000) {
+		$('.menu-icon,.themepack').hide(500);
+		$('.gototop').show(500);
+	}
+	else {
+		$('.menu-icon,.themepack').show(500);
+		$('.gototop').hide(500)
+	}
+});
+//create animated click and goto
+$('.topnav a').click(function(){
 	e.preventDefault();
 	$('body,html').animate({
 	scrollTop: $(this.hash).offset().top
       }, 5000);
 	});
-//mailto
+$('.menu-icon,.fa-times').click(function(){
+	$('body').toggleClass('togglenav');
+})
+//mailto function on click Gmail
 $(function () {
   $('.send').on('click', function (event) {
       event.preventDefault();
     var email = 'buddhalimbu157@gmail.com';
     var subject = $('#subject').val();
-    var emailBody = $('#textarea').val();
-    window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   emailBody;
+    var msg = $('#textarea').val();
+    window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   msg;
   });
 });
+$('.btn1').click(function(){
+	var title = document.location.origin;
+	var open = "http://www.facebook.com/sharer/sharer.php?u=" + title;
+	window.open(open, "_blank");
+})
+$('.btn2').click(function(){
+	var title = document.location.origin;
+	var msg = 'Buddha Limbu Portfolio Site';
+	var all = "https://api.whatsapp.com/send?text=" + msg + " - " + title;
+	window.open(all,"_blank");
+})
+//dark mode
+$(document).ready(function(){
+	$("#nightmode").click(function(){$("body").toggleClass("darktheme")}),$("body").toggleClass(localStorage.toggled),$("#nightmode").click(function(){"darktheme"!=localStorage.toggled?($("body").toggleClass("darktheme",!0),localStorage.toggled="darktheme"):($("body").toggleClass("darktheme",!1),localStorage.toggled="")});});
+
+//javascript sliders
 var slideIndex = 0;
 showSlides();
-
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("myslider");
@@ -81,76 +89,5 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-
-$('.header').prepend('<div class="date-holder"><div id="month"/><div class="time-holder"><span id="hours"/>:<span id="minutes"/></div></div>');
-
-var months = ['January','February','March','May','June','July','August','September','October','Novermber','December'];
-var date = new Date();
-var month = months[date.getMonth()];
-var hour = date.getHours() % 12 || 12 ;
-var minute = date.getMinutes();
-var a = document.getElementById('month');
-var b = document.getElementById('hours');
-var c = document.getElementById('minutes');
-a.innerHTML = month;
-b.innerHTML = hour;
-c.innerHTML = minute;
-
-//dark mode
-$(document).ready(function(){
-	$("#nightmode").click(function(){$("body").toggleClass("darktheme")}),$("body").toggleClass(localStorage.toggled),$("#nightmode").click(function(){"darktheme"!=localStorage.toggled?($("body").toggleClass("darktheme",!0),localStorage.toggled="darktheme"):($("body").toggleClass("darktheme",!1),localStorage.toggled="")});});
-
- //type
-
-                  function write (obj, sentence, i, cb) {
-                    if (i != sentence.length) {
-                      setTimeout(function () {
-                        i++
-                        console.log('in timeout', i)
-                        obj.innerHTML = sentence.substr(0, i+1) +' <em aria-hidden="true"></em>';
-                        write(obj, sentence, i, cb)
-                      }, 120)
-                    } else {
-                      console.log(i)
-                      cb()
-                    }
-                  }
-                   function erase (obj, cb,i) {
-                   var sentence = obj.innerText
-                      if (sentence.length != 0) {
-                       setTimeout(function () {
-                       sentence = sentence.substr(0,sentence.length-1)
-                       console.log('in timeout', i)
-                       obj.innerText = sentence
-                       erase(obj, cb)
-                        }, 18/(i*(i/10000000)))
-                        } else {
-                        obj.innerText = " "
-                        cb()
-                     }
-                    }
-                    var typeline = document.querySelector("#typeline")
-
-                     function writeerase(obj, sentence, time, cb) {
-                      write(obj, sentence, 0, function () {
-                       setTimeout(function () {
-                       erase(obj, cb) }, time) })
-                       }
-
-                  var sentences = [
-                    "a Programmer. ",
-                    "a Web Dev. ",
-                    "a Blogger. ",
-                    "a Designer. "
-                  ]
-                    
-                  var counter = 0
-                  function loop () {
-                    var sentence = sentences[counter % sentences.length]
-                    writeerase(typeline, sentence, 1500, loop)
-                    counter++
-                  }
-
-                  loop() 	
 
 });
