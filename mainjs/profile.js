d2f93bcea8afb798d2402c00d0c3a26b8b6ef32d
buddhalimbu <53037805+buddhalimbu@users.noreply.github.com>
2020-07-29 23:17:55 +0545

    Update profile.js

--- mainjs/profile.js
+++ mainjs/profile.js
@@ -85,86 +85,8 @@ $(function () {
     var monthFormat = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"],
         postPerPage = 8;
 
-    $(".blog-feat").each(function () {
-        var nth = "https://1.bp.blogspot.com/-Al8VTaXtQBM/Xutv9A0CH-I/AAAAAAAADFo/VWJUtJc09pMsufQwwiXPpQTQ2BvamSaEwCLcBGAsYHQ/s320/no-img.png";
-        var relPost = $(this);
-        var relLabel = $(this).find("span").attr("data-label");
-        var relType = $(this).find("span").attr("data-type");
-        if (relType != undefined && relType.match("featured")) {
-            $.ajax({
-                url: "http://buddhalimbu.com.np/feeds/posts/default?orderby=published&alt=json-in-script&max-results=10",
-                type: "get",
-                dataType: "jsonp",
-                beforeSend: function () {
-                    relPost.append('<div class="wait"><div class="loading-icon"/></div>');
-                },
-                success: function (a) {
-                    relPost.find(".wait").remove();
-                    relPost.append('<div class="featured-post-wrapper"/>');
-                    if (a.feed.entry) {
-                        for (var t = 0; t < a.feed.entry.length; t++) {
-                            for (var s = a.feed.entry[t], l = 0; l < a.feed.entry[t].link.length; l++)
-                                if ("alternate" == a.feed.entry[t].link[l].rel) {
-                                    var relHref = a.feed.entry[t].link[l].href;
-                                    break;
-                                }
-                            if ("content" in s) var relSum = s.content.$t;
-                            else relSum = "summary" in b_rc ? s.summary.$t : "";
-                            170 < (relSum = relSum.replace(/<\S[^>]*>/g, "")).length && (relSum = relSum.substring(0, 100) + "...");
-                            var postContent = s.content.$t;
-                            if (postContent.indexOf("<img") > -1) {
-                                var relThumb = s.media$thumbnail.url;
-                            } else if (postContent.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/) != null) {
-                                var ytthumb = postContent.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop();
-                                if (ytthumb.length == 11) {
-                                    var relThumb = "'//img.youtube.com/vi/" + ytthumb + "/0.jpg'";
-                                }
-                            } else if (postContent.match(/src=(.+?[\.jpg|\.gif|\.png]")/) != null) {
-                                relThumb = postContent.match(/src=(.+?[\.jpg|\.gif|\.png]")/)[1];
-                            } else {
-                                relThumb = nth;
-                            }
-                            if (s.category != null) {
-                                for (cat = 0; cat < s.category.length; cat++) var relCat = s.category[cat].term;
-                            } else {
-                                relCat = "uncategorized";
-                            }
-                            var relTitle = s.title.$t,
-                                relAuthor = s.author[0].name.$t,
-                                p = s.published.$t,
-                                c = p.substring(0, 4),
-                                f = p.substring(5, 7),
-                                relTime = p.substring(8, 10),
-                                postTime = monthFormat[parseInt(f, 9)] + " " + relTime + ", " + c,
-                                text = "view all",
-                                bb = '<a class="simple-viewmore" href="/search/label/' + relTitle + '?&max-result=8">' + text + "</a>",
-                                relBlock =
-                                    '<div class="f-post"><div class="f-img"><a class="f-href" href="' +
-                                    relHref +
-                                    '"><img class="f-thumb" data-src="' +
-                                    relThumb +
-                                    '"/></a></div><div class="f-meta"><div class="f-label"><a href="/search/label/' +
-                                    relCat +
-                                    '?&max-result=10">' +
-                                    relCat +
-                                    '</a></div><div class="f-title"><h2><a href="' +
-                                    relHref +
-                                    '">' +
-                                    relTitle +
-                                    '</a></h2></div><div class="f-footer"><span class="f-author meta-author">' +
-                                    relAuthor +
-                                    '</span><span class="f-time meta-time">' +
-                                    postTime +
-                                    "</span></div></div></div>";
-                            relPost.find(".featured-post-wrapper").append(relBlock), $(".f-thumb").JqueryLazyImg();
-                        }
-                    } else {
-                        relPost.append('<div class="noposts"><span class="fas fa-burn"/> <span>no posts check your label once in widget</span></div>');
-                    }
-                },
-            });
-        }
-    });
+       $(".blog-feat").each(function(){var nth='https://1.bp.blogspot.com/-Al8VTaXtQBM/Xutv9A0CH-I/AAAAAAAADFo/VWJUtJc09pMsufQwwiXPpQTQ2BvamSaEwCLcBGAsYHQ/s320/no-img.png';var relPost=$(this);var relLabel=$(this).find("span").attr("data-label");var relType=$(this).find("span").attr("data-type");if(relType!=undefined&&relType.match("featured")){$.ajax({url:"http://buddhalimbu.com.np/feeds/posts/default?orderby=published&alt=json-in-script&max-results=10",type:"get",dataType:"jsonp",beforeSend:function(){relPost.append('<div class="wait"><div class="loading-icon"/></div>')},success:function(a){relPost.find('.wait').remove();relPost.append('<div class="featured-post-wrapper"/>');if(a.feed.entry){for(var t=0;t<a.feed.entry.length;t++){for(var s=a.feed.entry[t],l=0;l<a.feed.entry[t].link.length;l++)if("alternate"==a.feed.entry[t].link[l].rel){var relHref=a.feed.entry[t].link[l].href;break}if("content"in s)var relSum=s.content.$t;else relSum="summary"in b_rc?s.summary.$t:"";170<(relSum=relSum.replace(/<\S[^>]*>/g,"")).length&&(relSum=relSum.substring(0,100)+"...");var postContent=s.content.$t;if(postContent.indexOf("<img")>-1){var relThumb=s.media$thumbnail.url}else if(postContent.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/)!=null){var ytthumb=postContent.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop();if(ytthumb.length==11){var relThumb="'//img.youtube.com/vi/"+ytthumb+"/0.jpg'"}}else if(postContent.match(/src=(.+?[\.jpg|\.gif|\.png]")/)!=null){relThumb=postContent.match(/src=(.+?[\.jpg|\.gif|\.png]")/)[1]}else{relThumb=nth}if(s.category!=null){for(cat=0;cat<s.category.length;cat++)var relCat=s.category[cat].term}else{relCat="uncategorized"}var relTitle=s.title.$t,relAuthor=s.author[0].name.$t,p=s.published.$t,c=p.substring(0,4),f=p.substring(5,7),relTime=p.substring(8,10),postTime=monthFormat[parseInt(f,9)]+" "+relTime+", "+c,text='view all',bb='<a class="simple-viewmore" href="/search/label/'+relTitle+'?&max-result=8">'+text+"</a>",relBlock='<div class="f-post"><div class="f-img"><a class="f-href" href="'+relHref+'"><img class="f-thumb" data-src="'+relThumb+'"/></a></div><div class="f-meta"><div class="f-label"><a href="/search/label/'+relCat+'?&max-result=10">'+relCat+'</a></div><div class="f-title"><h2><a href="'+relHref+'">'+relTitle+'</a></h2></div><div class="f-footer"><span class="f-author meta-author">'+relAuthor+'</span><span class="f-time meta-time">'+postTime+'</span></div></div></div>';relPost.find(".featured-post-wrapper").append(relBlock),$('.f-thumb').JqueryLazyImg()}}else{relPost.append('<div class="noposts"><span class="fas fa-burn"/> <span>no posts check your label once in widget</span></div>')}}})}});
+
 
     $(".year").html(year);
 
