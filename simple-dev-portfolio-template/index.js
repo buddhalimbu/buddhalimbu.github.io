53af29f53606fa1ecaa73ead908a9d955b5fead0
Buddhalimbu <53037805+buddhalimbu@users.noreply.github.com>
2022-08-08 09:11:46 +0545

    Update index.js

--- simple-dev-portfolio-template/index.js
+++ simple-dev-portfolio-template/index.js
@@ -1,45 +1,81 @@
-//functions
-$(document).ready(function(){
-$('.ct').click(function(){
-    $('body').toggleClass('togglecontact');
-});
-$('.sharethis').click(function(){
-    $('body').toggleClass('toggleshare');
-    $('body').toggleClass('toggleop');
-})
-//contact me
-$('.ok').click(function(e){
+// JavaScript 
+//https://buddhalimbu.github.io
+window.onload = () => {
+const ct = document.querySelector('.ct');
+const shareThis = document.querySelector('.sharethis');
+   const sendmail = document.querySelector('.ok');
+   const fb = document.querySelector('#fb');
+   const tw = document.querySelector('#tw');
+   const lk = document.querySelector('#lk');
+  const tl = document.querySelector('#tl');
+    const pin = document.querySelector('#pin');
+    //get location title link
+   const title = document.location.origin;
+    
+    function allEventListener(){
+    ct.addEventListener("click", OpenContact);
+    shareThis.addEventListener("click", share);
+    sendmail.addEventListener("click", mail);
+    fb.addEventListener("click", facebookShare);
+    tw.addEventListener("click", twitterShare);
+    lk.addEventListener("click", linkedinShare);
+    tl.addEventListener("click", telegramShare);
+   pin.addEventListener("click", pinterestShare);
+
+}
+
+// call all evenlistner 
+    allEventListener();
+}
+
+function OpenContact(){
+    document.body.classList.toggle('togglecontact');
+    document.body.classList.remove('toggle');
+}
+
+function share(){
+    document.body.classList.toggle('toggle');   document.body.classList.remove('togglecontact');
+}
+
+
+function mail(e) {
     e.preventDefault();
-    var name = 'Buddha';
-    var email = 'buddhalimbu157@gmail.com';
-    var msg = $('#textarea').val();
-    var subject = $('#subject').val();
+    const name = 'Buddha';
+    const email = 'bsanbalimbi@gmail.com';
+    const msg = document.querySelector('#textarea').value;
+    const subject = document.querySelector('#subject').value;
     window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   msg;
-})
-//global share
-var title = document.location.origin;
-$('#fb').click(function(){
+
+}
+
+function facebookShare(){
     link = 'http://www.facebook.com/sharer.php?u=' + title;
     window.open(link,"_blank");
-});
-$('#tw').click(function(){
+}
+
+function twitterShare(){
     link = 'http://twitter.com/share?url=' + title;
     window.open(link,"_blank");
-});
-$('#lk').click(function(){
+}
+
+
+function linkedinShare(){
     link = 'http://www.linkedin.com/shareArticle?mini=true&amp;amp;url=' + title;
     window.open(link,"_blank");
-});
-$('#tl').click(function(){
+}
+
+
+function telegramShare(){
     link = 'https://telegram.me/share/url?url=' + title;
     window.open(link,"_blank");
-});
-$('#pin').click(function(){
+}
+
+
+function pinterestShare(){
     link = 'http://pinterest.com/pin/create/button/?url=' + title;
     window.open(link,"_blank");
-});
-$('#fb').click(function(){
-    link = 'https://api.whatsapp.com/send?text=' + title;
-    window.open(link,"_blank");
-});
-});
+}
+
+
+
+  
